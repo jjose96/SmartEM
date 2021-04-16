429fee2affe39c9c493d6ae2d210e7bc1a8ca7a8
jjose96 <josephjose.967@gmail.com>
2021-04-16 19:05:59 +0530

    Profile update

--- SmartEM/src/app/Board/dashboard/dashboard.component.ts
+++ SmartEM/src/app/Board/dashboard/dashboard.component.ts
@@ -31,12 +31,12 @@ setTimeout(function(){
 },3000);
 setTimeout(function(){
   $(document).ready(function(){
-  new Chart(document.getElementById("chart2"),{"type":"line","data":{"labels":["January","February","March","April","May","June","July","August","September","October","November","December"],"datasets":[{"label":"Usage Analysis","data":[152,163,160,157,161,164,165,163,164,167,166,168],"fill":false,"borderColor":"rgb(99, 203, 137)","lineTension":0.1}]},"options":{}});
+  new Chart(document.getElementById('chart2'),{"type":"line","data":{"labels":["January","February","March","April","May","June","July","August","September","October","November","December"],"datasets":[{"label":"Usage Analysis","data":[152,163,160,157,161,164,165,163,164,167,166,168],"fill":false,"borderColor":"rgb(99, 203, 137)","lineTension":0.1}]},"options":{}});
 });
 },3000);
   }
 
+
   ngOnInit(): void {
   }
-
 }
