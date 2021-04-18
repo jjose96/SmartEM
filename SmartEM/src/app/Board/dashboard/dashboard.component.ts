baa7f083725335a40243506fc4a013314e4305f2
jjose96 <josephjose.967@gmail.com>
2021-04-18 19:59:18 +0530

    deploy

--- SmartEM/src/app/Board/dashboard/dashboard.component.ts
+++ SmartEM/src/app/Board/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,22 +9,7 @@ import * as $ from 'jquery';
 export class DashboardComponent implements OnInit {
 
   constructor() {
-    // tslint:disable-next-line:only-arrow-functions
 
-// tslint:disable-next-line:only-arrow-functions
-setTimeout(function(){
-  // tslint:disable-next-line:only-arrow-functions
-  $(document).ready(function(){
-  new Chart(document.getElementById("chart1"),{"type":"line","data":{'labels':["January","February","March","April","May","June","July"],"datasets":[{"label":"Usage Analysis","data":[1065,1059,1080,1081,1056,1055,1040],"fill":false,"borderColor":"rgb(99, 203, 137)","lineTension":0.1}]},"options":{}});
-});
-}, 3000);
-// tslint:disable-next-line:only-arrow-functions
-setTimeout(function(){
-  // tslint:disable-next-line:only-arrow-functions
-  $(document).ready(function(){
-  new Chart(document.getElementById('chart2'),{'type':"line","data":{"labels":["January","February","March","April","May","June","July","August","September","October","November","December"],"datasets":[{"label":"Usage Analysis","data":[152,163,160,157,161,164,165,163,164,167,166,168],"fill":false,"borderColor":"rgb(99, 203, 137)","lineTension":0.1}]},"options":{}});
-});
-}, 3000);
   }
 
 
--- SmartEM/src/app/Consumers/usages/usages.component.ts
+++ SmartEM/src/app/Consumers/usages/usages.component.ts
@@ -8,19 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class UsagesComponent implements OnInit {
 
   constructor() {
-    (function()
-    {
-      if( window.localStorage )
-      {
-        if( !localStorage.getItem('firstLoad') )
-        {
-          localStorage['firstLoad'] = true;
-          window.location.reload();
-        }
-        else
-          localStorage.removeItem('firstLoad');
-      }
-    })();
+    setTimeout(function() {
+      $(document).ready(function() {
+          new Chart(document.getElementById("chart2"), { "type": "line", "data": { "labels": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], "datasets": [{ "label": "Usage Analysis", "data": [152, 163, 160, 157, 161, 164, 165, 163, 164, 167, 166, 168], "fill": false, "borderColor": "rgb(99, 203, 137)", "lineTension": 0.1 }] }, "options": {} });
+      });
+  }, 3000);
   }
 
   ngOnInit(): void {
